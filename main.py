0681e766cdd2b6b81d10d6a8ec1e21ef9020711a
Jacob Rose <jacob@rostor.cloud>
2025-06-16 11:24:45 +0200

    added the abilitiy to set the content via CLI

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import os
+import sys
 from dotenv import load_dotenv
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
 from google import genai
 
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-model = 'gemini-2.0-flash-001'
-contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-response = client.models.generate_content(
-    model=model, contents=contents
-)
+    if len(sys.argv) == 1:
+        print("Please enter a prompt!")
+        exit(1)
 
-print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    contents = sys.argv[1]
+
+    model = 'gemini-2.0-flash-001'
+    response = client.models.generate_content(
+        model=model, contents=contents
+    )
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if __name__ == "__main__":
+    main()
