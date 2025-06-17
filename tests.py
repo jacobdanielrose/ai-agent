4be82fc4f0fcb404f60ce99b7c03ad125f8deb3e
Jacob Rose <jacob@rostor.cloud>
2025-06-17 22:27:54 +0200

    added function to write files.

--- tests.py
+++ tests.py
@@ -1,37 +1,20 @@
-from functions.get_file_content import get_file_content
-from functions.get_files_info import get_files_info
-
+from functions.write_file import write_file
 
 def test():
-    # result = get_files_info("calculator", ".")
-    # print("Result for current directory:")
-    # print(result)
-    # print("")
-
-    # result = get_files_info("calculator", "pkg")
-    # print("Result for 'pkg' directory:")
-    # print(result)
-
-    # result = get_files_info("calculator", "/bin")
-    # print("Result for '/bin' directory:")
-    # print(result)
-
-    # result = get_files_info("calculator", "../")
-    # print("Result for '../' directory:")
-    # print(result)
-
-
-    result = get_file_content("calculator", "main.py")
-    print("Result for 'main.py' file:")
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    print("Result for writing file lorem.txt:")
     print(result)
+    print("")
 
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print("Result for 'pkg/calculator.py' file:")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print("Result for writing file pkg/morelorem.txt:")
     print(result)
+    print("")
 
-    result = get_file_content("calculator", "/bin/cat")
-    print("Result for '/bin/cat' file:")
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print("Result for writing file /tmp/temp.txt:")
     print(result)
+    print("")
 
 if __name__ == "__main__":
     test()
