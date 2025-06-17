fadea43feac88e82db9f982a92c769f16b7a2e60
Jacob Rose <jacob@rostor.cloud>
2025-06-17 09:12:18 +0200

    added the ability to grab file information

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
+
 
 def main():
     load_dotenv()
@@ -12,15 +15,24 @@ def main():
         print("Please enter a prompt!")
         exit(1)
 
-    contents = sys.argv[1]
+    user_prompt = sys.argv[1]
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
     model = 'gemini-2.0-flash-001'
     response = client.models.generate_content(
-        model=model, contents=contents
+        model=model, contents=messages
     )
+
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if "--verbose" in sys.argv[1:]:
+        print(f"User prompt: {user_prompt}")
+        if response.usage_metadata:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
