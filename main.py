865b7a4662b72418b19973f6668fbad94b936f0d
Jacob Rose <jacob@rostor.cloud>
2025-06-18 10:47:25 +0200

    added prompt config

--- main.py
+++ main.py
@@ -21,12 +21,53 @@ def main():
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    model = 'gemini-2.0-flash-001'
-    response = client.models.generate_content(
-        model=model, contents=messages
+
+    schema_get_files_info = types.FunctionDeclaration(
+        name="get_files_info",
+        description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+        parameters=types.Schema(
+            type=types.Type.OBJECT,
+            properties={
+                "directory": types.Schema(
+                    type=types.Type.STRING,
+                    description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+                ),
+            },
+        ),
     )
 
-    print(response.text)
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+
+    config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+    )
+
+    model_name = 'gemini-2.0-flash-001'
+    response = client.models.generate_content(
+        model=model_name,
+        contents=messages,
+        config=config,
+    )
+
+    if response.function_calls is not None:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(response.text)
 
     if "--verbose" in sys.argv[1:]:
         print(f"User prompt: {user_prompt}")
